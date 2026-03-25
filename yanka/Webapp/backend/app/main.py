56e6602622fea0857d2998291c92b63e8e88613e
Aayush Bharti <aayush.bharti35@gmail.com>
2026-03-25 16:38:12 -0700

    Changes to build

--- yanka/Webapp/backend/app/main.py
+++ yanka/Webapp/backend/app/main.py
@@ -60,9 +60,20 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+_frontend_origin = os.getenv("FRONTEND_URL", "http://localhost:3000").rstrip("/")
+CORS_ORIGINS = list(
+    dict.fromkeys(
+        (
+            _frontend_origin,
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+        )
+    )
+)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
